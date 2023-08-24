44e70e834fab3c02727b409dff54b3ccf73c80a2
Mini Mathew <miniedattu@gmail.com>
2023-08-24 16:05:11 +0800

    init commit

--- test_functions.py
+++ test_functions.py
@@ -1,11 +1,19 @@
-def add(a, b):
-    return a + b
 
-def subtract(a, b):
-    return a + b  # <--- fix this in step 7
+from functions import add, subtract, multiply
+from functions import convert_fahrenheit_to_celsius as f2c
+import pytest
 
-def multiply(a, b):
-    return a * b
+def test_add():
+    assert add(2, 3) == 5
+    assert add('space', 'ship') == 'spaceship'
 
-def convert_fahrenheit_to_celsius(fahrenheit):
-    return multiply(subtract(fahrenheit, 32), 9 / 5) # <-- Fix this in step 7
+# uncomment the following test in step 5
+#def test_subtract():
+#    assert subtract(2, 3) == -1
+
+# uncomment the following test in step 11
+# def test_convert_fahrenheit_to_celsius():
+#    assert f2c(32) == 0
+#    assert f2c(122) == pytest.approx(50)
+#    with pytest.raises(AssertionError):
+#        f2c(-600)
